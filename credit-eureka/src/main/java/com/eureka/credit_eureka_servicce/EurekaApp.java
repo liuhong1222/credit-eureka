fa335df3c095e4303e8ef8c3def9b7e49d1ecbe6
liuhong1222 <evan_liu1222@163.com>
2023-02-07 18:25:52 +0800

    首次提交

---
+++ credit-eureka/src/main/java/com/eureka/credit_eureka_servicce/EurekaApp.java
@@ -0,0 +1,27 @@
+package com.eureka.credit_eureka_servicce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApp extends SpringBootServletInitializer
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(EurekaApp.class, args);
+    }
+    
+    @Override  
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {  
+        builder.sources(this.getClass());  
+        return super.configure(builder);  
+    }  
+}
